90f37b30922587de24674371112012f0aaf5d1bd
sabano3so2 <sabano3so2@gmail.com>
2021-12-24 12:02:20 +0900

    Initial Commit

---
+++ mixitoraing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  mixitoraing
+//
+//  Created by Masayuki WATANABE on 2021/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ mixitoraing/mixitoraingApp.swift
@@ -0,0 +1,17 @@
+//
+//  mixitoraingApp.swift
+//  mixitoraing
+//
+//  Created by Masayuki WATANABE on 2021/12/24.
+//
+
+import SwiftUI
+
+@main
+struct mixitoraingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
